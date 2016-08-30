d5021a408d3bda5ffdb5a7e092dd5c8785132119
thimoonxy <thimoon@sina.com>
2016-08-30 16:42:39 +0800

    using httputil instead of calling res.headers - curl.go

--- curl.go
+++ curl.go
@@ -6,9 +6,9 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"os"
 	"regexp"
-	//	"strconv"
 )
 
 func main() {
@@ -20,9 +20,9 @@ func main() {
 	urh := regexp.MustCompile("(https?://)?(\\S+)/?")
 	url := os.Args[1]
 	parse := urh.FindStringSubmatch(url)
-	proto := parse[1]
+	scheme := parse[1]
 	host := parse[2]
-	if len(proto) == 0 {
+	if len(scheme) == 0 {
 		url = "http://" + url
 	}
 	ip, _ := net.LookupIP(host)
@@ -32,8 +32,8 @@ func main() {
 
 	req.Header.Add("User-Agent",
 		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36")
-	req.Header.Add("Accept",
-		"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+	//	req.Header.Add("Accept",
+	//		"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
 	//	req.Header.Add("Accept-Encoding",
 	//		"gzip, deflate, sdch, br")
 
@@ -47,18 +47,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Resoved: ", ip)
+	dump, _ := httputil.DumpRequest(req, true)
+	fmt.Println(string(dump))
 
-	fmt.Printf("%s %s %s\n", req.Method, req.URL, req.Proto)
-	fmt.Printf("Host: %s\n", host)
-	fmt.Printf("Address: %v\n", ip)
-	for k, v := range req.Header {
-		fmt.Println(string(k)+":", v[0])
-	}
-	fmt.Printf("\n")
-	fmt.Printf("%s %s\n", res.Proto, res.Status)
-	for k, v := range res.Header {
-		fmt.Println(string(k)+":", v[0])
-	}
+	dump, _ = httputil.DumpResponse(res, true)
+	fmt.Println(string(dump))
+
+	//	fmt.Println(req.ContentLength)
 
 	if os.Args[2] == "-p" || os.Args[2] == "--print" {
 		fmt.Println(string(outputs))

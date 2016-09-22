99bb77129078e97b2917d8e1122b6bc0447893bb
thimoonxy <thimoon@sina.com>
2016-09-22 19:06:23 +0800

    add ArgsParse() and enable sorting for outputs.

--- logana/logana.go
+++ logana/logana.go
@@ -3,11 +3,14 @@ package main
 import (
 	"bufio"
 	"compress/gzip"
+	"flag"
 	"fmt"
 	"os"
 	"practiceGo/logana/src/logline"
 	"practiceGo/logana/src/output"
 	"reflect"
+	"sort"
+	"time"
 )
 
 func FieldCounter(buf *bufio.Reader, starttime int64, endtime int64, fieldname string, depth int) (map[interface{}]float64, map[interface{}]float64, float64) {
@@ -65,6 +68,26 @@ func FieldCounter(buf *bufio.Reader, starttime int64, endtime int64, fieldname s
 	return counter, stat, sum
 }
 
+func ArgsParse() (int, string, int64, int64, bool) {
+	//default timestamps
+	now := time.Now()
+	default_end_str := now.Format("2006 Jan 02 15:04:05 -0700")
+	default_start := now.Add(time.Duration(-60) * time.Second)
+	default_start_str := default_start.Format("2006 Jan 02 15:04:05 -0700")
+	//flags
+	depth := flag.Int("depth", 1, "Depth of url or reference. Show whole URL when <=0 .")
+	fieldname := flag.String("fieldname", "Hostname", "Avail Fieldnames: Remote_host, Log_name, Auth_user, URL, Protocol, Reference, User_agent, Method, Raw_data, Scheme, Hostname, ReferenceHostname, ReferenceScheme, Time_stamp, Status, Response_size ")
+	starttime_str := flag.String("start_time", default_start_str, "query from this start_time")
+	endtime_str := flag.String("end_time", default_end_str, "query till this end_time")
+	all := flag.Bool("all", false, "Show all outputs.")
+	flag.Parse()
+	//parse time flags
+	s_time, _ := time.Parse("2006 Jan 02 15:04:05 -0700", *starttime_str)
+	e_time, _ := time.Parse("2006 Jan 02 15:04:05 -0700", *endtime_str)
+	start_time, end_time := s_time.Unix(), e_time.Unix()
+	return *depth, *fieldname, start_time, end_time, *all
+}
+
 func main() {
 	r, _ := os.Open("c:\\Users\\simon\\Downloads\\varnishncsa.log.5.gz")
 	defer r.Close()
@@ -73,21 +96,32 @@ func main() {
 	//	l := &io.LimitedReader{R: buf, N: 20}
 	//	io.Copy(ioutil.Discard, l.R)
 
-	depth := 2
-	fieldname := "Hostname"
-	counter, stat, sum := FieldCounter(buf, 1452599529, 1452599530, fieldname, depth)
-	//	fmt.Println(counter)
-	//	fmt.Println(stat)
-	//	fmt.Println(sum)
+	depth, fieldname, start_time, end_time, all := ArgsParse()
+	counter, stat, sum := FieldCounter(buf, start_time, end_time, fieldname, depth)
+	//		./logana.exe -start_time "2016 Jan 12 06:52:02 -0500" -end_time "2016 Jan 12 06:54:12 -0500" -fieldname  Status
 
 	slice := new(output.Output_slice)
 	slice.Output_slice_gen(counter, stat, sum)
 	fmt.Printf("Percent        Count          Field - %s  \n", fieldname)
 
 	fmt.Println("_______________________________________________")
-	for _, record := range slice.Records {
-		fmt.Printf("%6s        %6.0f          %s  \n", record.Fmt_Percent, record.Number, record.Name)
+	sort.Sort(output.Records(slice.Records))
+	Records := slice.Records
+	if len(Records) <= 20 || all == true {
+		for _, record := range Records {
+			fmt.Printf("%6s        %6.0f          %v  \n", record.Fmt_Percent, record.Number, record.Name)
+		}
+	} else if len(Records) > 20 {
+		for i := 0; i < 10; i++ {
+			fmt.Printf("%6s        %6.0f          %v  \n", Records[i].Fmt_Percent, Records[i].Number, Records[i].Name)
+		}
+		fmt.Println("...... Records more than 20 ......")
+		fmt.Println("...... Truncated .................")
+		for i := len(Records) - 10; i < len(Records); i++ {
+			fmt.Printf("%6s        %6.0f          %v  \n", Records[i].Fmt_Percent, Records[i].Number, Records[i].Name)
+		}
+		fmt.Printf("\nUse --all parm to show all records queried.\n\n")
 	}
 	fmt.Println("_______________________________________________")
-	fmt.Printf("Totally %.0f lines during the qurey time period.\n", slice.Sum)
+	fmt.Printf("Totally %.0f log lines during the query time period.\n", slice.Sum)
 }
--- logana/src/output/output.go
+++ logana/src/output/output.go
@@ -16,9 +16,11 @@ func (record *Output_record) Format_percent() {
 }
 
 type Output_slice struct {
-	Records []Output_record
-	Sum     float64
+	Records
+	Sum float64
 }
+
+type Records []Output_record
 
 func (s *Output_slice) Output_slice_gen(counter map[interface{}]float64, stat map[interface{}]float64, sum float64) {
 	s.Sum = sum
@@ -32,4 +34,15 @@ func (s *Output_slice) Output_slice_gen(counter map[interface{}]float64, stat ma
 		record.Format_percent()
 		s.Records = append(s.Records, record)
 	}
+}
+
+func (s Records) Less(i, j int) bool {
+	return s[j].Percent < s[i].Percent
+}
+
+func (a Records) Len() int { // 重写 Len() 方法
+	return len(a)
+}
+func (a Records) Swap(i, j int) { // 重写 Swap() 方法
+	a[i], a[j] = a[j], a[i]
 }
